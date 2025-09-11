a6dce29a2c20d4d5d4308b80afb9aff717eae1fc
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-09-11 00:22:15 -0500

    wf

--- src/auth/mfa.ts
+++ src/auth/mfa.ts
@@ -16,8 +16,8 @@ export async function startTotpEnrollment() {
 
   // Return a "fake" finalize function
   return {
-    secret: "TypeIn1234",
-    qrCodeUrl: "1234",
+    secret: "Type: Qr1234",
+    qrCodeUrl: "Qr1234",
     finalize: async (code: string) => {
       console.warn('MFA TOTP finalize skipped.');
     },
