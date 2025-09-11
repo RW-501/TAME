c516137b08e749503c563e079adf55052e0c22ad
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-09-10 19:14:01 -0500

    up

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,1 +1,10 @@
-export const firebaseConfig={ apiKey:'REPLACE_ME', authDomain:'REPLACE_ME.firebaseapp.com', projectId:'REPLACE_ME', storageBucket:'REPLACE_ME.appspot.com', messagingSenderId:'', appId:'', databaseURL:'https://REPLACE_ME-default-rtdb.firebaseio.com' };
+export const firebaseConfig = {
+  apiKey: "AIzaSyDN0lPIkSuPybu5tFEvnRqzryPCsP5JU0A",
+  authDomain: "tame-b2463.firebaseapp.com",
+  databaseURL: "https://tame-b2463-default-rtdb.firebaseio.com",
+  projectId: "tame-b2463",
+  storageBucket: "tame-b2463.firebasestorage.app",
+  messagingSenderId: "906027510456",
+  appId: "1:906027510456:web:0de936e84cea8ff039624d"
+};
+
