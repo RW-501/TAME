4791cc14509df24448e413223c7af47dc528a8d5
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-09-10 23:04:38 -0500

    up

---
+++ src/assets/books/index.ts
@@ -0,0 +1,14 @@
+// C:\Users\lilro\Desktop\TAME\TAME\src\assets\books\index.ts
+export const manifests = [
+  {
+    id: "dc2a-short-o-pdf",
+    title: "Dan’s Hot Dogs — Short o (2A)",
+    schedule: "2A",
+    files: [
+      "Dan's Hot Dogs Answer Key.pdf",
+      "Dan's Hot Dogs double sided copy.pdf",
+      "Dan's Hot Dogs single sided.pdf",
+      "Printing Directions copy.pdf"
+    ]
+  }
+];
